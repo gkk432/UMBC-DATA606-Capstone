cadeaf4735ffb0dfa5df519ac1600efae9857e55
gkk432 <124212578+gkk432@users.noreply.github.com>
2024-12-09 01:47:51 -0500

    Update Streamlit.py

--- app/Streamlit.py
+++ app/Streamlit.py
@@ -8,16 +8,16 @@ def download_file_from_google_drive(file_id, output_file):
     url = f"https://drive.google.com/uc?id={file_id}"
     gdown.download(url, output_file, quiet=False)
 
-# Google Drive file ID for the model
+# Google Drive file ID for the Logistic Regression model
 model_file_id = "1SPWyi5617p3SSQ7Zfo3XPpJGiv_DbWLU"
 
-# Download model if not already present
+# Check if the model file is already downloaded
 if not os.path.exists("logistic_regression_model.pkl"):
     download_file_from_google_drive(model_file_id, "logistic_regression_model.pkl")
 
 # Load the model and vectorizer
 model = joblib.load("logistic_regression_model.pkl")
-tfidf_vectorizer = joblib.load("tfidf_vectorizer.pkl")  # Load the vectorizer locally
+tfidf_vectorizer = joblib.load("tfidf_vectorizer.pkl")  # Vectorizer is stored locally
 
 # Streamlit app UI
 st.title("Sentiment Analysis App")
